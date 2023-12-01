d4a6ae08a5478d2700867969cefabea8a4af87f7
ramya29p <152590160+ramya29p@users.noreply.github.com>
2023-12-01 17:54:22 +0530

    Update streamlit_app.py
    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
